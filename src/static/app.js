b3b1e889032031d63131445902d3f01d2b985f95
bowen-anth <bowen.anth@gmail.com>
2023-12-14 15:30:30 -0700

    fetching product data and console logging

--- src/static/app.js
+++ src/static/app.js
@@ -1,6 +1,22 @@
 window.addEventListener("DOMContentLoaded", setup);
 
+// Sort products by price low to high
+
+// If time allows, make UI components interactive (e.g. hovering over possible clickable items)
+
 async function setup() {
 	// START HERE
 	// PRODUCTS CAN BE FETCHED USING: GET /products
+
+	// thx google for fetch. rusty on this
+	fetch("./products")
+	.then(response => {
+		if(response.ok) {
+			return response.json();
+		}
+	}).then(data => {
+		if(data) {
+			console.log(data);
+		}
+	}).catch(err => console.error(err));
 }
